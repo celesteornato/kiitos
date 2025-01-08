5c9ee36ac2e16510c2fe2c515ad66dd844445b1f
celesteornato <celeste.ornato1@gmail.com>
2025-01-09 00:35:01 +0100

    reformated

--- src/kernel/gdt.c
+++ src/kernel/gdt.c
@@ -6,35 +6,33 @@
 struct segment_descriptor g_gdt[GDT_SIZE];
 struct gdt_descriptor g_gdtr;
 
-void gdt_set_descriptor(int num, uint64_t base, uint64_t limit, uint8_t access, uint8_t gran)
-{
-    g_gdt[num].base_low = (base & 0xFFFF);
-    g_gdt[num].base_middle = (base >> 16) & 0xFF;
-    g_gdt[num].base_high = (base >> 24) & 0xFF;
+void gdt_set_descriptor(int num, uint64_t base, uint64_t limit, uint8_t access,
+                        uint8_t gran) {
 
-    g_gdt[num].limit_low = (limit & 0xFFFF);
-    g_gdt[num].granularity = ((limit >> 16) & 0x0F);
-
-    g_gdt[num].granularity |= (gran & 0xF0);
-    g_gdt[num].access = access;
+  g_gdt[num] = (struct segment_descriptor){
+      .base_low = (base & 0xFFFF),
+      .base_middle = (base >> 16) & 0xFF,
+      .base_high = (base >> 24) & 0xFF,
+      .limit_low = (limit & 0xFFFF),
+      .granularity = ((limit >> 16) & 0x0F) | (gran & 0xf0),
+      .access = access,
+  };
 }
 
-void gdt_install()
-{
+void gdt_install() {
 
-    gdt_set_descriptor(0, 0, 0, 0, 0); // null desc
+  // null desc
+  gdt_set_descriptor(0, 0, 0, 0, 0);
 
-    // 64 bit code and data
-    gdt_set_descriptor(1, 0, 0x0, 0b10011011, 0b10100000);
-    gdt_set_descriptor(2, 0, 0x0, 0b10010111, 0b10000000);
-    
+  // 64 bit code and data
+  gdt_set_descriptor(1, 0, 0x0, 0b10011011, 0b10100000);
+  gdt_set_descriptor(2, 0, 0x0, 0b10010111, 0b10000000);
 
-    g_gdtr = (struct gdt_descriptor){
-        .limit = (sizeof(struct segment_descriptor)*GDT_SIZE)-1,
-        .base = (uint64_t)g_gdt,
-    };
+  g_gdtr = (struct gdt_descriptor){
+      .limit = (sizeof(struct segment_descriptor) * GDT_SIZE) - 1,
+      .base = (uint64_t)g_gdt,
+  };
 
-    load_gdt(g_gdtr.limit, g_gdtr.base);
-    reload_segments();
+  load_gdt(g_gdtr.limit, g_gdtr.base);
+  reload_segments();
 }
-		
--- src/kernel/idt.h
+++ src/kernel/idt.h
@@ -1,27 +1,14 @@
 #include <stdint.h>
-// struct interrupt_descriptor {
-//   uint16_t offset_low;
-//   uint16_t seg_selector;
-//   uint8_t ist: 3;
-//   uint8_t reserved_low: 5;
-//   uint8_t gate_type: 4;
-//   uint8_t zero_bit: 1;
-//   uint8_t dpl: 2;
-//   uint8_t present: 1;
-//   uint16_t mid_offset;
-//   uint32_t offset_high;
-//   uint32_t reserved;
-// } __attribute__((packed));
-
 
 struct interrupt_descriptor {
-	uint16_t    isr_low;      // The lower 16 bits of the ISR's address
-	uint16_t    kernel_cs;    // The GDT segment selector that the CPU will load into CS before calling the ISR
-	uint8_t	    ist;          // The IST in the TSS that the CPU will load into RSP; set to zero for now
-	uint8_t     attributes;   // Type and attributes; see the IDT page
-	uint16_t    isr_mid;      // The higher 16 bits of the lower 32 bits of the ISR's address
-	uint32_t    isr_high;     // The higher 32 bits of the ISR's address
-	uint32_t    reserved;     // Set to zero
+  uint16_t isr_low;         // 0 - 15
+  uint16_t kernel_cs;       // 16 - 31
+  uint8_t ist : 3;          // 32 - 34
+  uint8_t reserved_low : 5; // 35 - 39
+  uint8_t attributes;       // 40 - 47
+  uint16_t isr_mid;         // 48 - 63
+  uint32_t isr_high;        // 64 - 95
+  uint32_t reserved_high;   // 96 - 127
 } __attribute__((packed));
 
 struct idtr {
