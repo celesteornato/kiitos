bba2d3f7f4c2c229867010c1e7f9c325260e617f
celesteornato <celeste.ornato1@gmail.com>
2025-07-15 11:05:09 +0200

    Made amd64_init account for new style and try to init vmm

--- src/amd64/amd64_init.c
+++ src/amd64/amd64_init.c
@@ -1,26 +1,25 @@
+#include "amd64/amd64_init.h"
+#include "amd64/debug/logging.h"
 #include "amd64/interrupts/idt.h"
-#include <amd64/amd64_init.h>
-#include <amd64/debug/logging.h>
-#include <amd64/memory/gdt.h>
-#include <fun/colors.h>
+#include "amd64/memory/gdt.h"
+#include "amd64/memory/manager/vmm.h"
+#include "fun/colors.h"
 
 void arch_init(void)
 {
-    logsf("AMD64", COLOR, BLUE | GREEN, D_BLUE);
+    // Extra linebreak is wanted and cosmetic
+    putsf("AMD64\n", COLOR, BLUE | GREEN, D_BLUE);
 
     __asm__ volatile("cli");
-
-    logs("\nSetting up GDT...");
+    puts("Setting up GDT...");
     gdt_init();
-    logs("\tGDT set!");
-    logs("\nSetting up IDT...");
+    puts("\tGDT set!");
+    puts("Setting up IDT...");
     idt_init();
-    logs("\tIDT set!");
+    puts("\tIDT set!");
     __asm__ volatile("sti");
 
-    volatile int a = 1;
-    volatile int b = 0;
-    a /= b;
+    vmm_init();
 
-    logc('\n');
+    putc('\n');
 }
