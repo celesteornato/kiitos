0b1a9b626fdf4c9391670f835e28a1f45b7a9aa2
Céleste Ornato <celeste@ornato.com>
2025-11-15 23:16:20 +0100

    linker.ld: exposed some useful symbols

---
+++ include/meta/ldsymbols.h
@@ -0,0 +1,17 @@
+#ifndef LDSYMBOLS_H_
+#define LDSYMBOLS_H_
+
+
+
+#include <stdint.h>
+
+extern uint8_t ld_kernel_start;
+extern uint8_t ld_kernel_end;
+extern uint8_t ld_limine_start;
+extern uint8_t ld_limine_end;
+extern uint8_t ld_text_start;
+extern uint8_t ld_text_end;
+extern uint8_t ld_data_start;
+extern uint8_t ld_data_end;
+
+#endif //LDSYMBOLS_H_
